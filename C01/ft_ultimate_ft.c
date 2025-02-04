66f77f8cb3f75a7ba9b33d9a5d3c1a85726aa5b7
tsilvalisbon <127543582+tsilvalisbon@users.noreply.github.com>
2025-02-04 17:31:56 +0000

    Create ft_ultimate_ft.c

---
+++ C01/ft_ultimate_ft.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+void ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+/*
+#include <stdio.h>
+int main(void)
+{
+	int num = 0;
+	int *ptr1 = &num;
+	int **ptr2 = &ptr1;
+	int ***ptr3 = &ptr2;
+	int ****ptr4 = &ptr3;
+	int *****ptr5 = &ptr4;
+	int ******ptr6 = &ptr5;
+	int *******ptr7 = &ptr6;
+	int ********ptr8 = &ptr7;
+	int *********ptr9 = &ptr8;
+
+	printf("Antes: num = %d\n", num);
+
+	ft_ultimate_ft(ptr9);
+
+	printf("Depois: num = %d\n", num);
+	return 0;
+}
+*/
