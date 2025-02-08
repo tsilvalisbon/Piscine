52539cce764444f5096c0208b141149799dd8792
tsilvalisbon <127543582+tsilvalisbon@users.noreply.github.com>
2025-02-08 16:20:14 +0000

    Create ft_strcat.c

---
+++ C03/ft_strcat.c
@@ -0,0 +1,31 @@
+char *ft_strcat(char *dest, char *src)
+{
+    int i = 0;
+    int j = 0;
+
+    while (dest[i] != '\0')
+    {
+        i++;
+    }
+    
+    while (src[j] != '\0')
+    {
+        dest[i] =+ src[j];
+        i++;
+        j++;
+    }
+    
+    dest[i] = '\0';
+
+    return dest;
+}
+/*
+#include <stdio.h>
+int main()
+{
+    char dest[40] ="This is";
+    char src[] = " 42";
+
+    printf("Result: %s", ft_strcat(dest,src));
+}
+*/
