b140e5d4bfe173c1649de02dfb2da6803cdee6cc
tsilvalisbon <127543582+tsilvalisbon@users.noreply.github.com>
2025-02-13 14:08:53 +0000

    Create ft_putnbr.c

---
+++ C04/ft_putnbr.c
@@ -0,0 +1,27 @@
+void	ft_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+  unsigned int number;
+
+  if (nb < 0)
+  {
+        ft_putchar('-');
+        number = nb * -1;
+  }
+  else
+        number = nb;
+  if (number >= 10)
+        ft_putnbr(number / 10);
+  ft_putchar(number % 10 + 48);
+}
+/*
+int main()
+{
+    int nbr = -230;
+    ft_putnbr(nbr);
+}
+*/
