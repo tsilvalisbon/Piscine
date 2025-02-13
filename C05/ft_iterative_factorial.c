78aa8c9f13a5c43e0543f8b841184bb635fe8765
tsilvalisbon <127543582+tsilvalisbon@users.noreply.github.com>
2025-02-13 16:02:02 +0000

    Create ft_iterative_factorial.c

---
+++ C05/ft_iterative_factorial.c
@@ -0,0 +1,20 @@
+int ft_iterative_factorial(int nb)
+{
+    int i = 1;
+    int result = 1;
+    
+    while(nb >= i)
+    {
+        result *= i;
+        i++;
+    }
+    return (result);
+}
+/*
+#include <stdio.h>
+int main ()
+{
+    printf("O fatorial é 5\n");
+    printf("O resultado do fatorial é %d",ft_iterative_factorial(5));
+}
+*/
