3f328c21c4e15bec74159b02c637b8bc079e52ba
tsilvalisbon <127543582+tsilvalisbon@users.noreply.github.com>
2025-02-14 17:46:22 +0000

    Update ft_is_prime.c

--- C05/ft_is_prime.c
+++ C05/ft_is_prime.c
@@ -18,6 +18,11 @@ int ft_is_prime(int nb)
 /*
 int main ()
 {
-  printf("%d", ft_is_prime(5));
+  printf("%d\n", ft_is_prime(1));
+  printf("%d\n", ft_is_prime(2));
+  printf("%d\n", ft_is_prime(3));
+  printf("%d\n", ft_is_prime(4));
+  printf("%d\n", ft_is_prime(9));
+  printf("%d\n", ft_is_prime(11));
 }
 */
