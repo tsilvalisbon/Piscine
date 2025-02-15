97949029718aa3f549a9b9e1468b8cc56734872f
tsilvalisbon <127543582+tsilvalisbon@users.noreply.github.com>
2025-02-15 18:23:04 +0000

    Create ft_print_program_name.c

---
+++ C06/ft_print_program_name.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+int main(int argc, char **argv)
+{
+    int i = 0;
+
+    if (argc > 0)
+    {
+        while(argv[0][i] != '\0')
+        {
+            write(1, &argv[i], 1);
+            i++;
+        }
+        write (1, "\n", 1);
+    }
+}
