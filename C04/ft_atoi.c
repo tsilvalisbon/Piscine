b2c3497a17c7fbfeced991f50f65a58f3168d1e6
tsilvalisbon <127543582+tsilvalisbon@users.noreply.github.com>
2025-03-17 18:35:04 +0000

    Added Atoi

---
+++ C04/ft_atoi.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tomandra <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 19:03:56 by tomandra          #+#    #+#             */
+/*   Updated: 2025/03/08 18:38:24 by tomandra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
+{
+	int	result;
+	int	sign;
+	int	i;
+
+	result = 0;
+	sign = 1;
+	i = 0;
+	if (str[i] == '\0')
+		return (0);
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+/*
+#include <stdio.h>
+int main()
+{
+	char str[] = "";
+	int result = ft_atoi(str);
+	printf("%d", result);
+}*/
